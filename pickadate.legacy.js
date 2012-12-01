c96580e6a03063a3920e0104707469a2d124ebf4
Amsul <reach@amsul.ca>
2012-12-01 17:35:51 -0500

    legacy IE support added

--- pickadate.legacy.js
+++ pickadate.legacy.js
@@ -19,7 +19,6 @@
  */
 
 
-
 ;(function( $, window, document, undefined ) {
 
     'use strict';
@@ -43,11 +42,13 @@
          * Helper functions
          */
 
+        isLegacyIE = window.navigator.userAgent.match( 'MSIE 7.0' ),
+
         // Check if a value is a function
         // and trigger it, if that
-        triggerFunction = function( callback, scope, args ) {
-            if ( typeof callback == 'function' ) {
-                return callback.apply( scope, args )
+        triggerFunction = function( func, scope, args ) {
+            if ( typeof func == 'function' ) {
+                return func.call( scope, args )
             }
         },
 
@@ -1045,40 +1046,35 @@
              */
             function postRender() {
 
-                // Find and store the month selector
+                // Find the month selector and bind the change event
                 CALENDAR.selectMonth = $findInHolder( CLASSES.monthSelector ).on({
-
-                    // *** For iOS ***
-                    click: function( event ) { event.stopPropagation() },
-
-                    // Bind the change event
                     change: function() {
 
                         // Show the month based on the option selected
-                        // while parsing as a float
                         showMonth( +this.value )
 
                         // Find the new month selector and focus back on it
                         $findInHolder( CLASSES.monthSelector ).focus()
-                    }
-                })[ 0 ]
-
-                // Find and store the year selector
-                CALENDAR.selectYear = $findInHolder( CLASSES.yearSelector ).on({
+                    },
 
                     // *** For iOS ***
-                    click: function( event ) { event.stopPropagation() },
+                    click: function( event ) { event.stopPropagation() }
+                })[ 0 ]
 
-                    // Bind the change event
+                // Find the year selector and bind the change event
+                CALENDAR.selectYear = $findInHolder( CLASSES.yearSelector ).on({
                     change: function() {
 
                         // Show the year based on the option selected
-                        // and month currently in focus while parsing as a float
+                        // and month currently in focus
                         showMonth( MONTH_FOCUSED.MONTH, +this.value )
 
                         // Find the new year selector and focus back on it
                         $findInHolder( CLASSES.yearSelector ).focus()
-                    }
+                    },
+
+                    // *** For iOS ***
+                    click: function( event ) { event.stopPropagation() }
                 })[ 0 ]
             } //postRender
 
@@ -1112,6 +1108,10 @@
                     CALENDAR.selectYear.tabIndex = 0
                 }
 
+                // If it's legacy IE, render a new calendar on open
+                if ( isLegacyIE ) {
+                    calendarRender()
+                }
 
                 // Bind all the events to the document
                 $document.on( 'click.P' + CALENDAR.id + ' focusin.P' + CALENDAR.id + ' keydown.P' + CALENDAR.id, onDocumentEvent )
@@ -1378,6 +1378,7 @@
     } //Picker.defaults
 
 
+
     /**
      * Extend jQuery
      */
@@ -1399,6 +1400,139 @@
             }
         })
     } //pickadate
+
+
+
+    /**
+     * Legacy browser support
+     */
+
+    /**
+     * Cross-Browser Split 1.1.1
+     * Copyright 2007-2012 Steven Levithan <stevenlevithan.com>
+     * Available under the MIT License
+     * http://blog.stevenlevithan.com/archives/cross-browser-split
+     */
+    var nativeSplit = String.prototype.split, compliantExecNpcg = /()??/.exec('')[1] === undefined
+    String.prototype.split = function(separator, limit) {
+        var str = this
+        if (Object.prototype.toString.call(separator) !== '[object RegExp]') {
+            return nativeSplit.call(str, separator, limit)
+        }
+        var output = [],
+            flags = (separator.ignoreCase ? 'i' : '') +
+                    (separator.multiline  ? 'm' : '') +
+                    (separator.extended   ? 'x' : '') +
+                    (separator.sticky     ? 'y' : ''),
+            lastLastIndex = 0,
+            separator2, match, lastIndex, lastLength
+        separator = new RegExp(separator.source, flags + 'g')
+        str += ''
+        if (!compliantExecNpcg) {
+            separator2 = new RegExp('^' + separator.source + '$(?!\\s)', flags)
+        }
+        limit = limit === undefined ? -1 >>> 0 : limit >>> 0
+        while (match = separator.exec(str)) {
+            lastIndex = match.index + match[0].length
+            if (lastIndex > lastLastIndex) {
+                output.push(str.slice(lastLastIndex, match.index))
+                if (!compliantExecNpcg && match.length > 1) {
+                    match[0].replace(separator2, function () {
+                        for (var i = 1; i < arguments.length - 2; i++) {
+                            if (arguments[i] === undefined) {
+                                match[i] = undefined
+                            }
+                        }
+                    })
+                }
+                if (match.length > 1 && match.index < str.length) {
+                    Array.prototype.push.apply(output, match.slice(1))
+                }
+                lastLength = match[0].length
+                lastLastIndex = lastIndex
+                if (output.length >= limit) {
+                    break
+                }
+            }
+            if (separator.lastIndex === match.index) {
+                separator.lastIndex++
+            }
+        }
+        if (lastLastIndex === str.length) {
+            if (lastLength || !separator.test('')) {
+                output.push('')
+            }
+        } else {
+            output.push(str.slice(lastLastIndex))
+        }
+        return output.length > limit ? output.slice(0, limit) : output
+    }
+
+
+    // isArray support
+    if ( !Array.isArray ) {
+        Array.isArray = function( value ) {
+            return {}.toString.call( value ) == '[object Array]'
+        }
+    }
+
+
+    // Map array support
+    if ( ![].map ) {
+        Array.prototype.map = function ( callback, self ) {
+            var array = this, len = array.length, newArray = new Array( len )
+            for ( var i = 0; i < len; i++ ) {
+                if ( i in array ) {
+                    newArray[ i ] = callback.call( self, array[ i ], i, array )
+                }
+            }
+            return newArray
+        }
+    }
+
+
+    // Filter array support
+    if ( ![].filter ) {
+        Array.prototype.filter = function( callback ) {
+            if ( this == null ) throw new TypeError()
+            var t = Object( this ), len = t.length >>> 0
+            if ( typeof callback != 'function' ) throw new TypeError()
+            var newArray = [], thisp = arguments[ 1 ]
+            for ( var i = 0; i < len; i++ ) {
+              if ( i in t ) {
+                var val = t[ i ]
+                if ( callback.call( thisp, val, i, t ) ) newArray.push( val )
+              }
+            }
+            return newArray
+        }
+    }
+
+
+    // Index of array support
+    if ( ![].indexOf ) {
+        Array.prototype.indexOf = function( searchElement ) {
+            if ( this == null ) throw new TypeError()
+            var t = Object( this ), len = t.length >>> 0
+            if ( len === 0 ) return -1
+            var n = 0
+            if ( arguments.length > 1 ) {
+                n = Number( arguments[ 1 ] )
+                if ( n != n ) {
+                    n = 0
+                }
+                else if ( n != 0 && n != Infinity && n != -Infinity ) {
+                    n = ( n > 0 || -1 ) * Math.floor( Math.abs( n ) )
+                }
+            }
+            if ( n >= len ) return -1
+            var k = n >= 0 ? n : Math.max( len - Math.abs( n ), 0 )
+            for ( ; k < len; k++ ) {
+                if ( k in t && t[ k ] === searchElement ) return k
+            }
+            return -1
+        }
+    }
 
 
 
