4e5ae617f1726ca11a45ab5a80191291bdc21b1e
Amsul <reach@amsul.ca>
2013-04-23 17:29:55 -0400

    time picker docs

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -9,10 +9,15 @@
 
 
 
+/* ==========================================================================
+   Date picker demos
+   ========================================================================== */
+
+
 /**
  * Weekday labels
  */
-$( '#demo__weekdaysShort' ).pickadate({
+$( '#date_demo__weekdaysShort' ).pickadate({
     weekdaysShort: [ 'Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa' ],
     showMonthsShort: true
 })
@@ -22,7 +27,7 @@ $( '#demo__weekdaysShort' ).pickadate({
 /**
  * Translations
  */
-$( '#demo__translations' ).pickadate({
+$( '#date_demo__translations' ).pickadate({
     monthsFull: [ 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre' ],
     monthsShort: [ 'Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec' ],
     weekdaysShort: [ 'Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam' ],
@@ -35,7 +40,7 @@ $( '#demo__translations' ).pickadate({
 /**
  * Buttons
  */
-$( '#demo__buttons' ).pickadate({
+$( '#date_demo__buttons' ).pickadate({
     // Any false-y value will hide the button.
     today: '',
     clear: 'Clear selection'
@@ -46,12 +51,12 @@ $( '#demo__buttons' ).pickadate({
 /**
  * Formats
  */
-var $demoFormats = $( '#demo__formats' ).pickadate({
+$( '#date_demo__formats' ).pickadate({
     format: 'You selecte!d: dddd, dd mmm, yyyy',
     formatSubmit: 'yyyy-mm-dd',
     hiddenSuffix: '--submit',
     onSet: function() {
-        $demoFormats.
+        this.$node.
             closest( '.js__fieldset' ).
             after( '<div class="section__block section__block--notification-green">' +
                 '<p>Value to submit: <code>' +
@@ -66,11 +71,11 @@ var $demoFormats = $( '#demo__formats' ).pickadate({
 /**
  * Dropdown selectors
  */
-$( '#demo__selectors--a' ).pickadate({
+$( '#date_demo__selectors--a' ).pickadate({
     selectYears: true,
     selectMonths: true
 })
-$( '#demo__selectors--b' ).pickadate({
+$( '#date_demo__selectors--b' ).pickadate({
     selectYears: 4
 })
 
@@ -79,7 +84,7 @@ $( '#demo__selectors--b' ).pickadate({
 /**
  * First weekday
  */
-$( '#demo__firstDay' ).pickadate({
+$( '#date_demo__firstDay' ).pickadate({
     firstDay: 1
 })
 
@@ -90,19 +95,19 @@ $( '#demo__firstDay' ).pickadate({
  */
 
 // Limits as specific dates using JavaScript date objects.
-$( '#demo__limits--a' ).pickadate({
+$( '#date_demo__limits--a' ).pickadate({
     min: new Date(2013,3,20),
     max: new Date(2013,7,14)
 })
 
 // Limits as specific dates using arrays.
-$( '#demo__limits--b' ).pickadate({
+$( '#date_demo__limits--b' ).pickadate({
     min: [2013,3,20],
     max: [2013,7,14]
 })
 
 // Limits as dates relative to today.
-$( '#demo__limits--c' ).pickadate({
+$( '#date_demo__limits--c' ).pickadate({
     min: -15,
     max: true
 })
@@ -115,7 +120,7 @@ $( '#demo__limits--c' ).pickadate({
 
 // Disable a specific set of dates using arrays
 // formatted as [ YEAR, MONTH, DATE ].
-$( '#demo__disable-dates--a' ).pickadate({
+$( '#date_demo__disable-dates--a' ).pickadate({
     disable: [
         [2013,3,3],
         [2013,3,12],
@@ -126,7 +131,7 @@ $( '#demo__disable-dates--a' ).pickadate({
 
 // Disable an arbitrary set of dates using integers
 // from 1 - 7 representing the day of week.
-$( '#demo__disable-dates--b' ).pickadate({
+$( '#date_demo__disable-dates--b' ).pickadate({
     disable: [
         1, 4, 7
     ]
@@ -134,7 +139,7 @@ $( '#demo__disable-dates--b' ).pickadate({
 
 // Set the first array item to `true` to disable all the dates.
 // Then selectively enable specific or arbitrary sets of dates.
-$( '#demo__disable-dates--c' ).pickadate({
+$( '#date_demo__disable-dates--c' ).pickadate({
     disable: [
         true,
         1, 4, 7,
@@ -150,7 +155,7 @@ $( '#demo__disable-dates--c' ).pickadate({
 /**
  * Disable dates
  */
-$( '#demo__events' ).pickadate({
+$( '#date_demo__events' ).pickadate({
     onStart: function() {
         console.log( 'Hello there :)' )
     },
@@ -173,11 +178,166 @@ $( '#demo__events' ).pickadate({
 
 
 
+
+
+
+
+
+
+/* ==========================================================================
+   Time picker demos
+   ========================================================================== */
+
+
+/**
+ * Translations
+ */
+$( '#time_demo__translations' ).pickatime({
+    clear: 'effacer'
+})
+
+
+
+/**
+ * Buttons
+ */
+$( '#time_demo__buttons' ).pickatime({
+    // Any false-y value will hide the button.
+    clear: ''
+})
+
+
+
+/**
+ * Formats
+ */
+$( '#time_demo__formats' ).pickatime({
+    format: 'T!ime: HHi',
+    formatSubmit: 'HH:i',
+    hiddenSuffix: '--submit',
+    onSet: function() {
+        this.$node.
+            closest( '.js__fieldset' ).
+            after( '<div class="section__block section__block--notification-green">' +
+                '<p>Value to submit: <code>' +
+                    this.get({ select: 'yyyy/mm/dd' }) +
+                '</code></p></div>'
+            )
+    }
+})
+
+
+
+/**
+ * Time intervals
+ */
+$( '#time_demo__interval' ).pickatime({
+    interval: 150
+})
+
+
+
+/**
+ * Limits
+ */
+
+// Limits as specific times using arrays.
+$( '#time_demo__limits--a' ).pickatime({
+    min: [7,30],
+    max: [14,0]
+})
+
+// Limits as times relative to now.
+$( '#time_demo__limits--b' ).pickatime({
+    min: -5,
+    max: true
+})
+
+
+
+/**
+ * Disable times
+ */
+
+// Disable a specific set of times using arrays
+// formatted as [ HOUR, MINUTE ].
+$( '#time_demo__disable-times--a' ).pickatime({
+    disable: [
+        [0,30],
+        [2,0],
+        [8,30],
+        [9,0]
+    ]
+})
+
+// Disable an arbitrary set of times using integers
+// from 0 - 23 representing the hours.
+$( '#time_demo__disable-times--b' ).pickatime({
+    disable: [
+        3, 5, 7
+    ]
+})
+
+// Set the first array item to `true` to disable all the times.
+// Then selectively enable specific or arbitrary sets of times.
+$( '#time_demo__disable-times--c' ).pickatime({
+    disable: [
+        true,
+        3, 5, 7,
+        [0,30],
+        [2,0],
+        [8,30],
+        [9,0]
+    ]
+})
+
+
+
+/**
+ * Disable dates
+ */
+$( '#time_demo__events' ).pickatime({
+    onStart: function() {
+        console.log( 'Hello there :)' )
+    },
+    onRender: function() {
+        console.log( 'Whoa.. rendered anew' )
+    },
+    onOpen: function() {
+        console.log( 'Opened up' )
+    },
+    onClose: function() {
+        console.log( 'Closed now' )
+    },
+    onStop: function() {
+        console.log( 'See ya.' )
+    },
+    onSet: function( event ) {
+        console.log( 'Just set stuff:', event )
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+/* ==========================================================================
+   API stuff
+   ========================================================================== */
+
+
+
 /**
  * API demo: open-close
  */
 var $button_open_close = $( '#button__api-open-close' ),
-    $input_open_close = $( '#demo__api-open-close' ).pickadate({
+    $input_open_close = $( '#date_demo__api-open-close' ).pickadate({
         onOpen: function() {
             $button_open_close.text( 'Close' )
         },
@@ -202,7 +362,7 @@ $button_open_close.on( 'click', function( event ) {
  * API demo: isOpen
  */
 var $button_isOpen = $( '#button__api-isOpen' ),
-    $input_isOpen = $( '#demo__api-isOpen' ).pickadate({
+    $input_isOpen = $( '#date_demo__api-isOpen' ).pickadate({
         onOpen: function() {
             alert( 'The picker is open: ' + this.isOpen() )
         }
@@ -219,7 +379,7 @@ $button_isOpen.on( 'click', function( event ) {
  * API demo: start-stop
  */
 var $button_start_stop = $( '#button__api-start-stop' ),
-    $input_start_stop = $( '#demo__api-start-stop' ).pickadate({
+    $input_start_stop = $( '#date_demo__api-start-stop' ).pickadate({
         onStart: function() {
             $button_start_stop.text( 'Stop' )
         },
@@ -245,7 +405,7 @@ var manglePicker = function( picker ) {
             prepend('<p class="text-center">Hello there! Today is <code>' + todayString + '</code></p>')
     },
     $button_render = $( '#button__api-render' ),
-    $input_render = $( '#demo__api-render' ).pickadate(),
+    $input_render = $( '#date_demo__api-render' ).pickadate(),
     picker_render = $input_render.pickadate( 'picker' )
 $button_render.on( 'click', function( event ) {
     if ( $button_render.text() == 'Render' ) {
@@ -265,7 +425,7 @@ $button_render.on( 'click', function( event ) {
  * API demo: clear
  */
 var $button_clear = $( '#button__api-clear' ),
-    $input_clear = $( '#demo__api-clear' ).pickadate(),
+    $input_clear = $( '#date_demo__api-clear' ).pickadate(),
     picker_clear = $input_clear.pickadate( 'picker' )
 $button_clear.on( 'click', function( event ) {
     picker_clear.clear()
@@ -280,7 +440,7 @@ $button_clear.on( 'click', function( event ) {
 
 //value
 var $button_get__a = $( '#button__api-get--a' ),
-    $input_get__a = $( '#demo__api-get--a' ).pickadate(),
+    $input_get__a = $( '#date_demo__api-get--a' ).pickadate(),
     picker_get__a = $input_get__a.pickadate( 'picker' )
 $button_get__a.on( 'click', function( event ) {
     console.log( picker_get__a.get() )
@@ -288,7 +448,7 @@ $button_get__a.on( 'click', function( event ) {
 })
 
 //select
-var $input_get__b = $( '#demo__api-get--b' ).pickadate(),
+var $input_get__b = $( '#date_demo__api-get--b' ).pickadate(),
     picker_get__b = $input_get__b.pickadate( 'picker' )
 $( '#button__api-get--b' ).on( 'click', function( event ) {
     console.log( picker_get__b.get( 'select' ) )
