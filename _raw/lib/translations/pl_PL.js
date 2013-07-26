b6d895026534b2f2394ba50c1178971dd7fadf7c
Amsul <reach@amsul.ca>
2013-07-26 17:02:08 -0400

    translation + link fixes

--- _raw/lib/translations/pl_PL.js
+++ _raw/lib/translations/pl_PL.js
@@ -3,7 +3,7 @@
 $.extend( $.fn.pickadate.defaults, {
     monthsFull: [ 'styczeń', 'luty', 'marzec', 'kwiecień', 'maj', 'czerwiec', 'lipiec', 'sierpień', 'wrzesień', 'październik', 'listopad', 'grudzień' ],
     monthsShort: [ 'sty', 'lut', 'mar', 'kwi', 'maj', 'cze', 'lip', 'sie', 'wrz', 'paź', 'lis', 'gru' ],
-    weekdaysFull: [ 'niedziela', 'poniedziałek', 'wtorek', 'środa', 'czwartek', 'piąąek', 'sobota' ],
+    weekdaysFull: [ 'niedziela', 'poniedziałek', 'wtorek', 'środa', 'czwartek', 'piątek', 'sobota' ],
     weekdaysShort: [ 'N', 'Pn', 'Wt', 'Śr', 'Cz', 'Pt', 'So' ],
     today: 'dzisiaj',
     clear: 'usunąć',
