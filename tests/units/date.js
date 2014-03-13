424f1a142c29dc5b2b5c1c627de7af4cad49bc08
Rolf Bjaanes <rolf@powow.no>
2014-03-13 14:20:58 +0100

    Failing test for string format dd.mm.yy

--- tests/units/date.js
+++ tests/units/date.js
@@ -1267,5 +1267,15 @@ test( '`data-value` to select, highlight, and view', function() {
     deepEqual( picker.get( 'view' ).obj, new Date(1988,7,1), 'Viewsets date' )
 })
 
+test( 'two digit years before 68 placed in 2000s', function() {
 
+  $DOM.append( $INPUT.clone().val( '19.03.14' ) )
+  var $input = $DOM.find( 'input' ).pickadate({
+    format: 'dd.mm.yy'
+  })
+  var picker = $input.pickadate( 'picker' )
 
+  deepEqual( picker.get( 'select' ).obj, new Date(2014,2,19), 'Selects date' )
+  deepEqual( picker.get( 'highlight' ).obj, new Date(2014,2,19), 'Highlights date' )
+  deepEqual( picker.get( 'view' ).obj, new Date(2014,2,1), 'Viewsets date' )
+})
