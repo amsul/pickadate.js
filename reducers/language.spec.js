8b77a9a2b05223bf152674c5f0a1f2a5cd6ec8d5
Amsul <reach@amsul.ca>
2016-09-28 23:34:13 -0400

    :white_check_mark: [languageReducer] Add the test for setting the language

---
+++ reducers/language.spec.js
@@ -0,0 +1,26 @@
+const ACTION        = require('constants/action')
+const LANGUAGE      = require('constants/language')
+
+let languageReducer = require('reducers/language')
+
+
+
+describe('/languageReducer', () => {
+
+
+  describe('#[ACTION.TYPE.SET_LANGUAGE]', () => {
+
+    it('sets the language', () => {
+
+      let state   = LANGUAGE.ENGLISH
+      let payload = { value: LANGUAGE.FRENCH }
+
+      languageReducer[ACTION.TYPE.SET_LANGUAGE](state, payload)
+        .should.eql(LANGUAGE.FRENCH)
+
+    })
+
+  })
+
+
+})
