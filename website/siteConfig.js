7c40c86ab13c0cc899a80f803c4586ba1d5fd781
amsul <reach@amsul.ca>
2019-01-06 19:11:53 -0500

    Use a light code highlighting theme

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -39,7 +39,7 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'atom-one-dark',
+    theme: 'tomorrow',
   },
   usePrism: ['html', 'js', 'jsx'],
 
