1502da16f7ed0019d69d4db269bd6ee4298e3130
amsul <reach@amsul.ca>
2019-01-09 21:54:51 -0500

    Merge the “date store” API into the picker

---
+++ __tests__/core.js
@@ -0,0 +1,23 @@
+// @flow
+
+// import pickadate from 'pickadate/core'
+
+// const sleep = duration => new Promise(resolve => setTimeout(resolve, duration))
+
+describe('create()', () => {
+  it('creates a picker', async () => {
+    // const picker = pickadate.create()
+    // expect(picker).toHaveProperty('store')
+    // expect(picker).toHaveProperty('setSelected')
+    // // expect(dateStore.getState()).toEqual()
+  })
+
+  describe('setSelected()', () => {
+    it('sets the selected date', () => {
+      // const picker = pickadate.create()
+      // expect(picker.store.getState().selected).toBeNull()
+      // picker.setSelected({ value: new Date(2019, 3, 1) })
+      // expect(picker.store.getState().selected).toEqual(new Date(2019, 3, 1))
+    })
+  })
+})
