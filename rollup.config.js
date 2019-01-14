a4be413bca1ef21f00f8bb6e23cc219b77dcde05
amsul <reach@amsul.ca>
2019-01-13 20:33:54 -0500

    Add the translations

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import flow from 'rollup-plugin-flow'
@@ -6,20 +7,20 @@ import nodeResolve from 'rollup-plugin-node-resolve'
 import nodeGlobals from 'rollup-plugin-node-globals'
 import svgo from 'rollup-plugin-svgo'
 
-const getBrowserConfig = (input, output) => ({
-  input: `lib/apis/${input}.js`,
-  output: [
-    {
-      file: `builds/${output}.js`,
-      format: 'umd',
-      exports: 'default',
-      name: 'pickadate',
-      globals: {
-        react: 'React',
-        'react-dom': 'ReactDOM',
-      },
+import pkg from './package.json'
+
+const getConfig = ({ input, output }) => ({
+  input,
+  output: {
+    ...output,
+    format: 'umd',
+    exports: 'default',
+    banner: `/* pickadate v${pkg.version}, @license MIT */`,
+    globals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
     },
-  ],
+  },
   external: ['react', 'react-dom'],
   plugins: [
     babel({
@@ -40,10 +41,34 @@ const getBrowserConfig = (input, output) => ({
   ],
 })
 
+const getCoreConfig = (input, output) =>
+  getConfig({
+    input: `lib/${input}.js`,
+    output: {
+      file: `builds/${output}.js`,
+      name: 'pickadate',
+    },
+  })
+
+const getTranslationConfig = translation => {
+  const path = `translations/${translation}`
+  return getConfig({
+    input: `lib/${path}`,
+    output: {
+      file: `builds/${path}`,
+      name: `pickadate.translations.${translation.replace(/\.js$/, '')}`,
+    },
+  })
+}
+
 export default [
-  getBrowserConfig('dom/index', 'index'),
-  getBrowserConfig('dom/vanilla', 'vanilla'),
-  getBrowserConfig('react-dom/index', 'react-dom'),
-  getBrowserConfig('react-native/index', 'react-native'),
-  getBrowserConfig('jquery/index', 'jquery'),
+  getCoreConfig('apis/dom/index', 'index'),
+  getCoreConfig('apis/dom/vanilla', 'vanilla'),
+  getCoreConfig('apis/react-dom/index', 'react-dom'),
+  getCoreConfig('apis/react-native/index', 'react-native'),
+  getCoreConfig('apis/jquery/index', 'jquery'),
+  ...fs
+    .readdirSync('./lib/translations')
+    .filter(name => /\.js$/.test(name))
+    .map(translation => getTranslationConfig(translation)),
 ]
