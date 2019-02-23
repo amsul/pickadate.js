379a8dc45a6de002599a292f9be3793684a33499
amsul <reach@amsul.ca>
2019-02-23 17:53:13 -0500

    Add the v5 docs

---
+++ v5/js/custom.js
@@ -0,0 +1,14 @@
+if (window.location.pathname === '/pickadate.js/v5/') {
+  document.getElementById('footer').style.display = 'none'
+} else {
+  document.getElementById('footer').style.display = ''
+}
+document.body.dataset.pathname = window.location.pathname
+
+//
+
+var element = document.getElementById('pickadate--datepicker')
+if (element) {
+  var picker = window.pickadate.create()
+  window.pickadate.render(element, picker)
+}
